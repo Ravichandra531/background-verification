5236644ecedc28cb72c7ad90dea3627eb74cfe0d
Ravichandra531 <ruchitrshinde@gmail.com>
2026-05-21 01:15:28 +0530

    fixing linting error

---
+++ backend/prisma/migrations/20260520194424_init/migration.sql
@@ -0,0 +1,59 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Candidate" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "aadhaarNumber" TEXT NOT NULL,
+    "panNumber" TEXT NOT NULL,
+    "dob" TIMESTAMP(3) NOT NULL,
+    "address" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdById" TEXT NOT NULL,
+
+    CONSTRAINT "Candidate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationLog" (
+    "id" TEXT NOT NULL,
+    "candidateId" TEXT NOT NULL,
+    "verificationType" TEXT NOT NULL,
+    "requestPayload" JSONB NOT NULL,
+    "responsePayload" JSONB NOT NULL,
+    "verificationStatus" TEXT NOT NULL,
+    "verifiedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "VerificationLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Candidate_email_idx" ON "Candidate"("email");
+
+-- CreateIndex
+CREATE INDEX "Candidate_status_idx" ON "Candidate"("status");
+
+-- CreateIndex
+CREATE INDEX "VerificationLog_candidateId_idx" ON "VerificationLog"("candidateId");
+
+-- AddForeignKey
+ALTER TABLE "Candidate" ADD CONSTRAINT "Candidate_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VerificationLog" ADD CONSTRAINT "VerificationLog_candidateId_fkey" FOREIGN KEY ("candidateId") REFERENCES "Candidate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
